00087e17eca0f8d4586b3e0f1319776d1c7f7619
20007888 <1519783291@qq.com>
2021-10-04 10:45:35 +0800

    first

---
+++ src/main/java/kkl/Cale.java
@@ -0,0 +1,19 @@
+public class Cale {
+    public static int add(int a, int b) {
+        int sum;
+        sum =a+b;
+        System.out.println(sum);
+        return sum;
+    }
+    public static int subtract(int c ,int d) {
+//        java.util.Scanner input = new java.util.Scanner(System.in);
+//        System.out.println("subtract 请输入a值");
+//        int a = input.nextInt();
+//        System.out.println("请输入b值");
+//        int b = input.nextInt();
+        int ssum;
+        ssum =c+d;
+        System.out.println(ssum);
+        return ssum;
+    }
+}
---
+++ src/test/java/kkl/CaleTest.java
@@ -0,0 +1,6 @@
+public class CaleTest {
+    public static void main(String[] args) {
+        assert Cale.add(5,9)==14:"5+9=14";
+        Cale.subtract(8,2);
+    }
+}
