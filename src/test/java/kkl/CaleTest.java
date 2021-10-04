a02a16583ff663e978515dba2e07eec747c901ff
20007888 <1519783291@qq.com>
2021-10-04 13:09:03 +0800

    fourth

--- src/test/java/kkl/CaleTest.java
+++ src/test/java/kkl/CaleTest.java
@@ -1,10 +1,17 @@
 package kkl;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CaleTest {
-    public static void main(String[] args) {
-        assertEquals(14,Cale.add(5,9));
+    @Test
+    public void addtest() {
+
+        assertEquals(14, Cale.add(5, 9));
+    }
+    @Test
+    public void subtracttest() {
         assertEquals(6,Cale.subtract(8,2));
 
     }
